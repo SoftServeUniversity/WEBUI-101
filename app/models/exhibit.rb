430aecb3ccdaa7626b8201ef4fa039d8e8208ea6
oleh05 <roomaroom@gmail.com>
2013-11-08 18:07:51 +0200

    remove length

--- app/models/exhibit.rb
+++ app/models/exhibit.rb
@@ -1,7 +1,5 @@
 class Exhibit < ActiveRecord::Base
-  validates :name, presence: true, length: { in: 2..50 }
-  validates :registration_number, presence: true, length: { in: 2..8 }
-  validates :opportunity_for_transportation, length: { maximum: 40 }
-  validates :the_degree_of_preservation, length: { maximum: 140 }
-  validates :description, length: { minimum: 3 }
+  validates :name, presence: true
+  validates :registration_number, presence: true
+
 end
