9d78d4b520d52344a5c848cee64b497f2c803311
vshchutc <victoria.schutska@gmail.com>
2013-11-12 15:27:50 +0200

    Datepicker is already finished

--- app/models/exhibition.rb
+++ app/models/exhibition.rb
@@ -1,16 +1,11 @@
 class Exhibition < ActiveRecord::Base
   geocoded_by :adress
   after_validation :geocode
+  validates_presence_of :name , :adress
+
   validate :start_date_cannot_be_later_than_end_date
- 
+
   def start_date_cannot_be_later_than_end_date
-  	# p '=============================================='
-  	# p start_date
-  	# p end_date
-  	# p sdate = Date.strptime(self.start_date, '%d-%m-%Y')
-  	# p edate = Date.strptime(self.end_date, '%d-%m-%Y')
-  	# p '=============================================='
-  	# binding.pry
     if   start_date > end_date 
       errors.add(:start_date, "Start date can't be greater than end date")
     end
