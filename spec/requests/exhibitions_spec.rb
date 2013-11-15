dee3709a128def77dafe6967b4ed3dd5a2f94f3e
vshchutc <victoria.schutska@gmail.com>
2013-11-15 11:28:52 +0200

    rspec

--- spec/requests/exhibitions_spec.rb
+++ spec/requests/exhibitions_spec.rb
@@ -6,9 +6,9 @@ describe "Exhibitions" do
     before do
       sign_in(user)
     end
-    xit "works! (now write some real specs)", js: true do
+    it "works! (now write some real specs)", js: true do
       visit exhibitions_path
-      binding.pry
+
       response.status.should be(200)
     end
   end
