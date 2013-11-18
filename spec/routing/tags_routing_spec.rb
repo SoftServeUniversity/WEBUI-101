e41c7901bce9acb41e0c916fe61ed7ad6287012b
akazar <ultragit@gmail.com>
2013-11-18 22:26:05 +0200

    Edit routing test for tags

--- spec/routing/tags_routing_spec.rb
+++ spec/routing/tags_routing_spec.rb
@@ -4,31 +4,31 @@ describe TagsController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/tags").should route_to("tags#index")
+      get("/admin/tags").should route_to("tags#index")
     end
 
     it "routes to #new" do
-      get("/tags/new").should route_to("tags#new")
+      get("/admin/tags/new").should route_to("tags#new")
     end
 
     it "routes to #show" do
-      get("/tags/1").should route_to("tags#show", :id => "1")
+      get("/admin/tags/1").should route_to("tags#show", :id => "1")
     end
 
     it "routes to #edit" do
-      get("/tags/1/edit").should route_to("tags#edit", :id => "1")
+      get("/admin/tags/1/edit").should route_to("tags#edit", :id => "1")
     end
 
     it "routes to #create" do
-      post("/tags").should route_to("tags#create")
+      post("/admin/tags").should route_to("tags#create")
     end
 
     it "routes to #update" do
-      put("/tags/1").should route_to("tags#update", :id => "1")
+      put("/admin/tags/1").should route_to("tags#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      delete("/tags/1").should route_to("tags#destroy", :id => "1")
+      delete("/admin/tags/1").should route_to("tags#destroy", :id => "1")
     end
 
   end
