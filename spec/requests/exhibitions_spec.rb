7be7aac144f10f80bd92e3c1b79a5ac832ce2313
vshchutc <victoria.schutska@gmail.com>
2013-11-19 11:15:28 +0200

    edit test

--- spec/requests/exhibitions_spec.rb
+++ spec/requests/exhibitions_spec.rb
@@ -6,10 +6,10 @@ describe "Exhibitions" do
     before do
       sign_in(user)
     end
-    it "works! (now write some real specs)", js: true do
+    it "works! (now write some real specs)" do
       visit exhibitions_path
 
-      response.status.should be(200)
+      page.status_code.should be(200)
     end
   end
 end
