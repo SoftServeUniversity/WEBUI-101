788e20848cddecd0b19ded0d5735efc33e5f433f
rpelyush <rpelyush@gmail.com>
2013-11-19 19:03:44 +0200

    pending test

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20131115183822) do
+ActiveRecord::Schema.define(version: 20131118145929) do
 
   create_table "exhibitions", force: true do |t|
     t.string   "name"
