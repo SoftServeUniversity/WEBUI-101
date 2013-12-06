3a3fd2dcce8c7fab5fe90e4a0e4b2c7dc90b26c4
vshchutc <victoria.schutska@gmail.com>
2013-12-06 12:14:56 +0200

    clean up

--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
 describe Property do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it{ should validate_presence_of :title }
+  it{ should validate_presence_of :value }
 end
