5b9bdaca46844aead9c4a96a5040d68c6aeb536e
rpelyush <rpelyush@gmail.com>
2013-12-11 20:33:32 +0200

    modify seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,18 +15,20 @@ User.create(email: 'user@example.com', password: 'helloworld', password_confirma
 end
 
 30.times do |n|
-  Exhibit.create(name: "Scout hat-#{n}",
+  exhibit = Exhibit.create(name: "Scout hat-#{n}",
                  registration_number: "170488#{n}",
                  description: 'Elephants are large mammals of the family Elephantidae and the order Proboscidea. Traditionally, two species are recognised, the African elephant (Loxodonta africana) and the Asian elephant (Elephas maximus), although some evidence suggests that African bush elephants and African forest elephants are separate species (L. africana and L. cyclotis respectively). Elephants are scattered throughout sub-Saharan Africa, South Asia, and Southeast Asia. Elephantidae are the only surviving family of the order proboscidea; other, now extinct, families of the order include mammoths and mastodons. ',
-                user_id: @user.id)
+                 user_id: @user.id)
+  exhibit.versions.each {|v| v.update_attribute('whodunnit', @user.id)}
 end
 
 30.times do |t|
-  Exhibition.create(name: "Exhibition of scout hats-#{t}",
+  exhibition = Exhibition.create(name: "Exhibition of scout hats-#{t}",
                   description: 'Everything what is not relevant to elephant is irrelephant.',
                   start_date: '20-11-2013',
                   end_date: '21-11-2013',
                   virtual: false,
                   adress: 'Lviv',
                   user_id: @user.id)
+  exhibition.versions.each {|v| v.update_attribute('whodunnit', @user.id)}
 end
