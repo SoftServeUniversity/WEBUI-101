10266ded8a68e5e46f41aefeb877c1269ce51f1f
rpelyush <rpelyush@gmail.com>
2013-12-13 00:47:13 +0200

    refac/modify search_controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,25 +1,42 @@
 class SearchController < ApplicationController
-  before_filter :prepend_view_paths
 
   def index
-    if params[:q]
-      @search = []
-      searchable_models.each do |model|
-        query = model.search { fulltext params[:q] }
-        @search << query if query.results.any?
-      end
+    if !params[:search]
+      render :index
     end
-    binding.pry
+
+    if params[:search] && params[:search][:q].present? && params[:search].count == 1
+      @search = perform_search_on(searchable_models)
+    end
+
+    if params[:search] && params[:search][:q].present? && !models_from_params.empty?
+      @search = perform_search_on(models_from_params)
+    end
   end
 
   private
 
     def searchable_models
-      ActiveRecord::Base.connection.tables.map{|x| x.classify.safe_constantize}.compact.select(&:searchable?)
+      # ActiveRecord::Base.connection.tables.map{|x| x.classify.safe_constantize}.compact.select(&:searchable?)
+      convert_to_models(ActiveRecord::Base.connection.tables)
     end
 
-    def prepend_view_paths
-      prepend_view_path "#{Rails.root}/app/views/search"
+    def models_from_params
+      # params[:search].to_a.map{|x| x.first.classify.safe_constantize}.compact.select(&:searchable?)
+      convert_to_models(params[:search].keys)
+    end
+
+    def convert_to_models(args)
+      args.map{|x| x.classify.safe_constantize}.compact.select(&:searchable?)
+    end
+
+    def perform_search_on(models)
+      search = []
+      models.each do |model|
+        query = model.search { fulltext params[:search][:q] }
+        search << query if query.results.any?
+      end
+      search
     end
 end
 
