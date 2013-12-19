0cae1cc1048eac2fde2b43af7ec8ed025715b7c4
rpelyush <rpelyush@gmail.com>
2013-12-19 10:55:22 +0200

    exhibits search

--- app/controllers/exhibits_controller.rb
+++ app/controllers/exhibits_controller.rb
@@ -4,7 +4,22 @@ class ExhibitsController < ApplicationController
   # GET /exhibits
   # GET /exhibits.json
   def index
-    @exhibits = Exhibit.all.page(params[:page]).per(10)
+    if params[:search].present?
+      @search = Exhibit.search do
+        fulltext params[:search] do
+          boost_fields name: 2.0
+        end
+        paginate page: params[:page], per_page: 10
+      end
+      @exhibits = @search.results
+
+    else
+      @exhibits = Exhibit.all.page(params[:page]).per(10)
+    end
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   # GET /exhibits/1
