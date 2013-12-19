dbe8b696ee051c2539e3f44ba22526d95b1292cd
rpelyush <rpelyush@gmail.com>
2013-12-19 18:58:40 +0200

    add exhibitions search

--- app/controllers/exhibitions_controller.rb
+++ app/controllers/exhibitions_controller.rb
@@ -1,13 +1,26 @@
 class ExhibitionsController < ApplicationController
   before_action :set_exhibition, only: [:show]
 
-    def index
-    @exhibitions = Exhibition.all.page(params[:page]).per(5)
+  def index
+    if params[:search].present?
+      @search = Exhibition.search do
+        fulltext params[:search] do
+          boost_fields name: 2.0
+        end
+        paginate page: params[:page], per_page: 5
+      end
+      @exhibitions = @search.results
+
+    else
+      @exhibitions = Exhibition.all.page(params[:page]).per(5)
+    end
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
-  def show
-
-  end
+  def show; end
   private
     def set_exhibition
       @exhibition = Exhibition.find(params[:id])
