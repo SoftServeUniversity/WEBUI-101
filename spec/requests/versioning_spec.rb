1de99c2bd2f25150147897cb101eb81bbf33cb94
rpelyush <rpelyush@gmail.com>
2013-12-24 20:53:28 +0200

    configure PaperTrail for using in specs

---
+++ spec/requests/versioning_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe 'Models versioning' do
+  let(:user) { FactoryGirl.create :user }
+
+  before(:each) do
+    sign_in(user)
+  end
+
+  describe 'testing', versioning: true do
+    let!(:exhibit) { FactoryGirl.create(:exhibit, name: 'scout') }
+
+    it 'works' do
+      visit admin_exhibit_versions_path(exhibit)
+      binding.pry
+      expect(page).to have_content('scout')
+    end
+  end
+end
