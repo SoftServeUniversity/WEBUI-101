69eef7935970295f8f804bedf52c6995c4cd38e3
auberonandrutovych <auberonandrutovych@gmail.com>
2013-12-24 15:04:44 +0400

    fix routing specs

--- spec/routing/biographies_routing_spec.rb
+++ spec/routing/biographies_routing_spec.rb
@@ -1,34 +1,34 @@
 require "spec_helper"
 
-describe BiographiesController do
+describe Admin::BiographiesController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/biographies").should route_to("biographies#index")
+      get("/admin/biographies").should route_to("admin/biographies#index")
     end
 
     it "routes to #new" do
-      get("/biographies/new").should route_to("biographies#new")
+      get("/admin/biographies/new").should route_to("admin/biographies#new")
     end
 
     it "routes to #show" do
-      get("/biographies/1").should route_to("biographies#show", :id => "1")
+      get("/admin/biographies/1").should route_to("admin/biographies#show", :id => "1")
     end
 
     it "routes to #edit" do
-      get("/biographies/1/edit").should route_to("biographies#edit", :id => "1")
+      get("/admin/biographies/1/edit").should route_to("admin/biographies#edit", :id => "1")
     end
 
     it "routes to #create" do
-      post("/biographies").should route_to("biographies#create")
+      post("/admin/biographies").should route_to("admin/biographies#create")
     end
 
     it "routes to #update" do
-      put("/biographies/1").should route_to("biographies#update", :id => "1")
+      put("/admin/biographies/1").should route_to("admin/biographies#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      delete("/biographies/1").should route_to("biographies#destroy", :id => "1")
+      delete("/admin/biographies/1").should route_to("admin/biographies#destroy", :id => "1")
     end
 
   end
