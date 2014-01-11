ff081195c49207150a69de74522975af7af416a9
ichyr <zmii@plast.org.ua>
2014-01-11 23:37:14 +0200

    added concrete assets to precompile

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,6 @@ module MuseumMs
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-    config.assets.precompile += ['*.css.erb', '*.css.scss', '*.js.coffee']
+    config.assets.precompile += ['admin.js', 'admin.css']
   end
 end
