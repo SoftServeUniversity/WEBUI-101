259b17fbfde1411d07210bf3da8765ccd992ef24
ichyr <zmii@plast.org.ua>
2014-01-12 17:09:45 +0200

    провівд холодний деплой

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ MuseumMs::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-  config.assets.precompile += [ 'admin.js', 'admin.css']
+  config.assets.precompile += [ 'admin/admin.js.coffee', 'admin/admin.css.scss']
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
